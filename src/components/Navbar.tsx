835fc550efe17b74c88165fae3548e49addc24a6
cmatteo <camilo.matteo@babelgroup.com>
2024-07-03 10:14:54 -0400

    :tada: first commit - mui implemented

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,100 @@
+import React, { useState } from "react"
+import { AppBar, Box, Divider, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material"
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import MenuIcon from '@mui/icons-material/Menu';
+
+interface Props {
+  children: React.ReactNode
+  window?: () => Window
+}
+
+const drawerWidth = 240
+const navItems = [
+  {
+    text: "Favorites",
+    icon: <FavoriteIcon />
+  }
+]
+
+const Navbar = ({ children, window }: Props) => {
+  const [mobileOpen, setMobileOpen] = useState<boolean>(false)
+
+  const handleToggleDrawer = () => {
+    setMobileOpen((prevState) => !prevState)
+  }
+
+  const drawer = (
+    <Box role="presentation" onClick={handleToggleDrawer}>
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item.text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {item.icon}
+              </ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <div>
+      <AppBar component="nav">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleToggleDrawer}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1 }}
+          >
+            DoggyEst
+          </Typography>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            {navItems.map((item) => (
+              <IconButton size="large" aria-label="favorites" color="inherit">
+                {item.icon}
+              </IconButton>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleToggleDrawer}
+          ModalProps={{
+            keepMounted: true
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth }
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+      <Box>
+        <Toolbar />
+        {children}
+      </Box>
+    </div>
+  );
+}
+
+export default Navbar
